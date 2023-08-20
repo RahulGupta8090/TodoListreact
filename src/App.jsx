27a5b59d47a66dedd94c71b037f277dd79de3dbc
RahulGupta8090 <142726189+RahulGupta8090@users.noreply.github.com>
2023-08-20 18:31:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import ToDoList from "./ToDoList";
+import './index.css'
+const App=()=>{
+  const[inputList,setInputList]=useState("buyApple");
+  const[Items,setItems]=useState([]);
+
+  const itemEvent=(event)=>{
+setInputList(event.target.value);
+  };
+
+  const listOfItems=()=>{
+    setItems((oldItems) =>{
+      return [...oldItems, inputList];
+    })
+    setInputList("");
+  }
+
+  const deleteItems=(id)=>{
+     console.log("deleted"); 
+     
+     setItems((oldItems)=>{
+      return oldItems.filter((arrElem,index)=>{
+        return index!==id;
+      })
+     })
+  }
+
+return(
+  <>
+  <div className="main_div">
+    <div className="center_div">
+     <br/>
+     <h1>ToDoList</h1>
+     <br/>
+     <input type="text" placeholder="Add Item" 
+     value={inputList} 
+     onChange={itemEvent}/>
+     <button onClick={listOfItems}>+</button>
+
+     <ol>
+      {/* <li>{inputList}</li> */}
+      { Items.map( (itemval,index) => {
+        return <ToDoList key={index} 
+        id={index} 
+        text={itemval}
+
+        onSelect={deleteItems}
+        />
+      })}
+     </ol>
+    </div>
+
+  </div>
+  </>
+)
+}
+export default App;
